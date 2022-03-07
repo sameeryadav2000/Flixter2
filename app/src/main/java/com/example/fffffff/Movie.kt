d91e9998ab814ee3eca3d907841e0a7ee33b7afa
sameeryadav2000 <sameeryadav6000@gmail.com>
2022-03-07 02:37:39 -0500

    Initial commit

---
+++ app/src/main/java/com/example/fffffff/Movie.kt
@@ -0,0 +1,35 @@
+package com.example.fffffff
+import org.json.JSONArray
+import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Movie(
+    val movieId: Int,
+    val voteAverage:Double,
+    private val posterPath: String,
+    val title: String,
+    val overview: String,
+) : Parcelable {
+    @IgnoredOnParcel
+    val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
+    companion object {
+        fun fromJsonArray(movieJsonArray: JSONArray): List<Movie> {
+            val movies = mutableListOf<Movie>()
+            for (i in 0 until movieJsonArray.length()) {
+                val movieJson = movieJsonArray.getJSONObject(i)
+                movies.add(
+                    Movie(
+                        movieJson.getInt("id"),
+                        movieJson.getDouble("vote_average"),
+                        movieJson.getString("poster_path"),
+                        movieJson.getString("title"),
+                        movieJson.getString("overview")
+                    )
+                )
+            }
+            return movies
+        }
+    }
+}
